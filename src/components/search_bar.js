f9aad1a2a6cb51a50cd29961d877430451b58f5d
Warrick Holfeld <warrick.holfeld@envestnet.com>
2018-04-24 23:38:25 -0700

    fineshed section 2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,10 +17,17 @@ class SearchBar extends Component {
           //event.persist();
           //console.log(event);
           //console.log(event.target.value);
-          this.setState({ term: event.target.value});
-        }} />
+          this.onInputChange(event.target.value)
+          //this.setState({ term: event.target.value});
+        }
+      } />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
